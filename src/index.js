c4cff782c2fa52dea5a0c930053c379e8ac320d7
Luke Lazzaro <zodiac44@hotmail.com>
2022-03-09 10:49:18 +1100

    Add Firebase to the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomeScreen from './HomeScreen';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8YxQEq1gwv85hxbtkeBA07DabMPuda9g",
+  authDomain: "twitter-clone-5f8b5.firebaseapp.com",
+  projectId: "twitter-clone-5f8b5",
+  storageBucket: "twitter-clone-5f8b5.appspot.com",
+  messagingSenderId: "172413252916",
+  appId: "1:172413252916:web:079c6825427783d94fa161"
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <HomeScreen />,
